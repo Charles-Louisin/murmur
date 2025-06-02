d6a6a0ae19aecc26b57e3a64120626a830dec9d0
Charles Louisin <clynlouisin@gmail.com>
2025-06-02 19:11:36 +0100

    Delete backend directory

--- backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login } from '../controllers/authController.js';
-
-const router = express.Router();
-
-// Route de connexion
-router.post('/login', login);
-
-export default router; 
